7000090e7a607990412afb379158e4e581e7852b
Girish Bhasin <grishbhasin@gmail.com>
2025-08-01 14:03:50 +0530

    Add ice_breaker.py

---
+++ ice_breaker.py
@@ -0,0 +1,14 @@
+
+
+
+# conda install langchain-community
+# conda install langchainhub
+# conda install requests
+# conda install langchain-openai
+
+
+
+
+
+if __name__ == "__main__":
+    print("Hello, langChain!")
